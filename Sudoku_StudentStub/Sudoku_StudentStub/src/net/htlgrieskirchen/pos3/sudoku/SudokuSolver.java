95a4e4ea48cba5902d92f9fd422cbc9bd92226e4
thomasegles <71641615+thomasegles@users.noreply.github.com>
2020-10-19 13:28:09 +0200

    readSudoku method code

--- Sudoku_StudentStub/Sudoku_StudentStub/src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
+++ Sudoku_StudentStub/Sudoku_StudentStub/src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
@@ -1,6 +1,10 @@
 package net.htlgrieskirchen.pos3.sudoku;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /* Please enter here an answer to task four between the tags:
  * <answerTask4>
@@ -8,19 +12,48 @@ import java.io.File;
  * </answerTask4>
  */
 public class SudokuSolver implements ISodukoSolver {
+
     public SudokuSolver() {
         //initialize if necessary
     }
 
     @Override
     public final int[][] readSudoku(File file) {
-        // implement this method
-        return new int[0][0]; // delete this line!
+        int[][] arr = new int[9][9];
+        try {
+            arr = Files.lines(file.toPath())
+                    .map(s -> s.split(";"))
+                    .map(a -> new int[]{Integer.parseInt(a[0]),
+                Integer.parseInt(a[1]),
+                Integer.parseInt(a[2]),
+                Integer.parseInt(a[3]),
+                Integer.parseInt(a[4]),
+                Integer.parseInt(a[5]),
+                Integer.parseInt(a[6]),
+                Integer.parseInt(a[7]),
+                Integer.parseInt(a[8])})
+                    .toArray(int[][]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return arr;
     }
 
     @Override
     public boolean checkSudoku(int[][] rawSudoku) {
-        // implement this method
+        int row = 0;
+        int col = 0;
+        int rowVal = 0;
+        int colVal = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = rowVal; j < row; j++) {
+                for (int k = colVal; k < col; k++) {
+                    //code
+                }
+                col = 0;
+                row++;
+            }
+        }
         return false; // delete this line!
     }
 
@@ -29,7 +62,7 @@ public class SudokuSolver implements ISodukoSolver {
         // implement this method
         return new int[0][0]; // delete this line!
     }
-    
+
     @Override
     public int[][] solveSudokuParallel(int[][] rawSudoku) {
         // implement this method
